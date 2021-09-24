987ac99102c840cfcc2865619fbe0e1151a011e4
Taqi <Taqi.1994@yahoo.com>
2021-09-24 15:29:00 -0400

    push

---
+++ src/main/java/org/example/ex26/months.java
@@ -0,0 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Taqyuldeen Alzubaidi
+ */
+
+
+package org.example.ex26;
+import java.util.Scanner;
+
+public class months
+{
+    public static int calculate ( double balance, double apr, double monthlypayment )
+    {
+
+        //give it this value:
+        apr = apr/365;
+        return (int)Math.ceil( (-1 / 30.0)*Math.log ( 1+balance/monthlypayment * ( 1 - Math.pow (1+apr,30) )  ) / Math.log(1 +apr)  );
+
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner reader = new Scanner (System.in);
+        int month;
+        double balance;
+        double apr;
+        double monthlypayment;
+
+        System.out.print("What is your balance? ") ;
+        balance=reader.nextDouble() ;
+
+        System.out.print("What is the APR on the card (as a percent)? ") ;
+        apr = reader.nextDouble();
+        apr = apr/100;
+
+        System.out.print("What is the monthly payment you can make? ");
+        monthlypayment = reader.nextDouble();
+
+        month = calculate ( balance, apr, monthlypayment );
+        System.out.println("It will take " + month + " months to pay off this card.");
+    }
+}
