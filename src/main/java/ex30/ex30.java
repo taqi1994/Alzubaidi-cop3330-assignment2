1f3fde9af623e93fab39a137d1910ecab3f5cf66
Taqi <Taqi.1994@yahoo.com>
2021-09-25 05:41:01 -0400

    push

---
+++ src/main/java/ex30/ex30.java
@@ -0,0 +1,30 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Taqyuldeen Alzubaidi
+ */
+
+
+package ex30;
+public class ex30
+{
+
+    public static void main(String[] args)
+    {
+        int x, y;
+
+
+        for ( x = 1 ; x <= 12 ; x++)
+
+        {
+            for( y = 1 ; y <= 12 ; y++ )
+
+            {
+                System.out.print( ( x * y )+"\t") ;
+
+            }
+            System.out.println();
+        }
+
+    }
+
+}
